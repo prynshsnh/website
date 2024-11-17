28b639a3689c3c1b19b083f53914a0e71bab79ad
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-11-17 12:36:51 +0530

    Meta changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://priyanshu.online",
+  site: "https://astro-website-ochre.vercel.app",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
