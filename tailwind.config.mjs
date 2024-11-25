aecb0fce7175ddebc203d258818b4ceb5ddbdbc6
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-11-25 13:52:01 +0530

    Removed animations and added postcss

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
