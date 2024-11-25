231964367a08ce16ffc88b2bcac1e8b6bbfef274
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-11-25 16:48:00 +0530

    undo config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: "class",
-	// content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	jit: true,
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			fontFamily: {
