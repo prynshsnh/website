584362cfff5d7b9ae2b4fcc31c5babc904419673
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-11-27 13:30:34 +0530

    Added spotify page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import sitemap from "@astrojs/sitemap";
 
 import compress from "astro-compress";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://priyanshusinha.in",
-	integrations: [tailwind(), sitemap(), mdx(), compress()],
-	markdown: {
-		shikiConfig: {
-			theme: "css-variables",
-		},
-	},
+    site: "https://priyanshusinha.in",
+    integrations: [tailwind(), sitemap(), mdx(), compress(), react()],
+    markdown: {
+        shikiConfig: {
+            theme: "css-variables",
+        },
+    },
 });
