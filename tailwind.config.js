bf8aca342062d2b6457031314be29a4c135af8f4
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-12-07 13:02:14 +0530

    Animation Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,7 @@ module.exports = {
       sans: "Suisse, -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif",
       serif: ["Tiempos", ...defaultTheme.fontFamily.serif],
       mono: "input-mono-condensed, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
-    },
-    extend: {
-      keyframes: {
-        'fade-in': {
-          "0%": { opacity: 0, transform: "translateY(20px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
-      },
-      animation: {
-        'fade-in': "fade-in 300ms cubic-bezier(0.16, 0, 0.13, 1)",
-      },
-    },
+    }
   },
   plugins: [require("@tailwindcss/typography")],
 }
