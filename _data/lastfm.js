5e7f0488eaeb3d1f0a0c8c9a8dd9cbfa7dd2a864
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2025-01-04 16:24:27 +0530

    Minor edits

--- _data/lastfm.js
+++ _data/lastfm.js
@@ -28,7 +28,7 @@ const fetchLastFm = async (method, duration, extraArgs) => {
 const fetchTopTracks = async (period = "7day") => {
   const response = await fetchLastFm(
     "user.gettoptracks",
-    "5m",
+    "10s",
     `period=${period}`,
   );
 
